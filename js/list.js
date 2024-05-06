4d79a27f13cd549b6616fcf5843aedd7ece8b1b3
rinox3005 <peppe.rinox@gmail.com>
2024-05-06 20:13:08 +0200

    [UPD] list styled

--- js/list.js
+++ js/list.js
@@ -9,7 +9,8 @@ const shoppingList = [
     'Latte',
     'Frutta',
     'Cereali',
-    'Salumi'];
+    'Salumi',
+    'Carne'];
 
 // inizializzazione variabile counter del ciclo
 let counter = 0;
@@ -21,8 +22,18 @@ const ul = document.getElementById('list');
 while (counter < shoppingList.length) {
     //creo un elemento li
     const li = document.createElement('li');
-    //inserisco l'elemento della lista della spesa alla posizione [counter]
-    li.innerText = shoppingList[counter];
+    //assegno delle classi ad li
+    li.classList.add('d-flex', 'flex-row', 'icon')
+    //creo un elemento i
+    const i = document.createElement('i');
+    //assegno delle classi al mio elemento i
+    i.classList.add('fa-regular', 'fa-lemon')
+    //creo un elemento h5
+    const h5 = document.createElement('h5');
+    //inserisco l'elemento della lista della spesa alla posizione [counter] dentro il mio h5
+    h5.innerText = shoppingList[counter];
+    //appendo al mio li gli elementi i e h5 creati in precedenza
+    li.append(i, h5)
     //appendo l'elemento li appena creato e riempito alla ul creata in precedenza
     ul.append(li);
     //incremento il contatore
