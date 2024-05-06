f097ca875671e0ca147f192d6b01b5d70f2f57d9
rinox3005 <peppe.rinox@gmail.com>
2024-05-06 17:54:48 +0200

    [UPD] created li element, filled, appended to ul and printed on screen

--- js/list.js
+++ js/list.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//lista della spesa
 const shoppingList = [
     'Curry',
     'Limoni',
@@ -10,9 +11,20 @@ const shoppingList = [
     'Cereali',
     'Salumi'];
 
+// inizializzazione variabile counter del ciclo
 let counter = 0;
 
+//prendo l'elemento con id list
+const ul = document.getElementById('list');
+
+//finché il valore di counter e minore della lunghezza della lista
 while (counter < shoppingList.length) {
-    console.log(shoppingList[counter]);
+    //creo un elemento li
+    const li = document.createElement('li');
+    //inserisco l'elemento della lista della spesa alla posizione [counter]
+    li.innerText = shoppingList[counter];
+    //appendo l'elemento li appena creato e riempito alla ul creata in precedenza
+    ul.append(li);
+    //incremento il contatore
     counter++;
 }
