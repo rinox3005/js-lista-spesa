512f06623808878b145ca5016e5d36a77bde6edf
rinox3005 <peppe.rinox@gmail.com>
2024-05-06 15:52:22 +0200

    [UPD] while cycle done, result printed in console

--- js/list.js
+++ js/list.js
@@ -1,2 +1,18 @@
 'use strict';
 
+const shoppingList = [
+    'Curry',
+    'Limoni',
+    'Acqua',
+    'Biscotti',
+    'Latte',
+    'Frutta',
+    'Cereali',
+    'Salumi'];
+
+let counter = 0;
+
+while (counter < shoppingList.length) {
+    console.log(shoppingList[counter]);
+    counter++;
+}
